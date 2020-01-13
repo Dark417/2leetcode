33040adb24dfe178b44c0b2a2b076a515bb5461c
Dark417 <dark417@163.com>
2020-01-13 11:16:54 -0500

    my watch begins

---
+++ py n.py
@@ -0,0 +1,23 @@
+
+
+x = 1 if 2>1 else 0
+
+
+x = x for x in y if x > 0
+
+
+.list() #convert to list
+
+.pop() #get the last one; remove it from the list
+
+
+a = ["1","2","5"]
+
+print ord(a.pop())
+print ord('0')
+
+n1 = ord(a.pop())-ord('0')
+
+print n1
+
+
